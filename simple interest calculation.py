9967c11e532f529d99a4592544f205c855a8061e
Shaik kalesha <112747888+Shaik7Kalesha@users.noreply.github.com>
2022-09-05 14:02:34 +0530

    Delete simple interest calculation.py

--- simple interest calculation.py
+++
@@ -1,8 +0,0 @@
-def simple_interest(p,t,r):
-    print("the principal is",p)
-    print("the time is",t)
-    print("the rate of interest is",r)
-    si=(p*t*r)/100
-    print("the sample_interest is",si)
-    return si
-simple_interest (10,2,3)
