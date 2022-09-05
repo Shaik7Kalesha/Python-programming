f1b15d3b24a49b1c68f2d567c13b48f9a9917152
Shaik kalesha <112747888+Shaik7Kalesha@users.noreply.github.com>
2022-09-05 13:15:51 +0530

    Update area of circle.py

--- area of circle.py
+++ area of circle.py
@@ -1,4 +1,4 @@
-a = 3.14
-r = float(input("Enter the radius of circle:"))
-b= a*r*r
-print("area of circle ",b)
+a=3.14
+r=float(input("Enter the radius of circle:"))
+b=a*r*r
+print("area of circle",b)
