ef2ee63088ec8e4fbb75fb63bbe10b8207ee2a34
Shaik kalesha <112747888+Shaik7Kalesha@users.noreply.github.com>
2022-09-05 22:46:00 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+str = 'aIbohPhoBiA'
+str = str.casefold()
+rev_str = reversed(str)
+if list(str) == list(rev_str):
+   print("The string is a palindrome.")
+else:
+   print("The string is not a palindrome.")
