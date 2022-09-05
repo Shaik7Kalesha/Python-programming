09f07cb68e56ae52247b7126ac4ad89f4fc15afd
Shaik kalesha <112747888+Shaik7Kalesha@users.noreply.github.com>
2022-09-05 22:55:08 +0530

    Add files via upload

---
+++ count number of vowels.py
@@ -0,0 +1,9 @@
+def vowel_count(str):
+    count = 0
+    vowel = set("aeiouAEIOU")
+    for alphabet in str:
+        if alphabet in vowel:
+            count = count + 1
+    print("No. of vowels :", count)
+str = "shaik kalesha"
+vowel_count(str)
