34da7a12f8eba31d09a703a9706d7e0dfde83dc9
Shaik kalesha <112747888+Shaik7Kalesha@users.noreply.github.com>
2022-09-06 12:14:32 +0530

    Delete Anagram.py

--- Anagram.py
+++
@@ -1,8 +0,0 @@
-def check(s1, s2):
-    if (sorted(s1) == sorted(s2)):
-        print("The strings are anagrams.")
-    else:
-        print("The strings aren't anagrams.")
-s1 = "listen"
-s2 = "silent"
-check(s1, s2)
