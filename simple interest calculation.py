daeae314cec3a4efeba7f8edaad0b97d43319afb
Shaik kalesha <112747888+Shaik7Kalesha@users.noreply.github.com>
2022-09-06 12:10:09 +0530

    Delete simple interest calculation.py

--- simple interest calculation.py
+++
@@ -1,7 +0,0 @@
-def simple_interest():
-    p=int(input("Enter the principal amount"))
-    t=int(input("Enter the time period "))
-    r=int(input("Enter the rate of interest"))
-    n=(p*t*r)/100
-    print(n)
-simple_interest()
