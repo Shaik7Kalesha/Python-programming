d0320e538faf7062ee3d6d996f5ace6dbc7fe1b2
Shaik kalesha <112747888+Shaik7Kalesha@users.noreply.github.com>
2022-09-06 11:18:14 +0530

    Update simple interest calculation.py

--- simple interest calculation.py
+++ simple interest calculation.py
@@ -1,5 +1,7 @@
-p=float(input("the principal amount is:"))
-t=float(input("the time period is:"))
-r=float(input("the rate of interest:"))
-a=(p*t*r)/100
-print("the simple interest is:",a)
+def simple_interest():
+    p=int(input("Enter the principal amount"))
+    t=int(input("Enter the time period "))
+    r=int(input("Enter the rate of interest"))
+    n=(p*t*r)/100
+    print(n)
+simple_interest()
